47d6fd697708d3be906612cd8cd2bb3650f6be71
Camilo Andres Nuñez <camilo.n14@outlook.com>
2024-04-03 10:44:57 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,1 +1,9 @@
+import nodes as nds
+import code_generator as cg
+import input_output as io
 
+#Write the path of the Latex file below
+path='/Users/camilonunez/Desktop/Arbol.tex'
+tree=io.Latex_to_string(path)
+content=cg.semantics(cg.ontology(cg.types(cg.parameters(tree),tree),tree),tree)
+io.coq_file(content,path)
